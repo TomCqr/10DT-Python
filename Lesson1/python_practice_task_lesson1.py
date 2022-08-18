f328830bbaee8544e17e4707f84de5e82500bee1
TomCqr <103461338+TomCqr@users.noreply.github.com>
2022-08-18 12:38:19 +1200

    Add files via upload

---
+++ Lesson1/python_practice_task_lesson1.py
@@ -0,0 +1,25 @@
+# Tom Collier
+# Practice task
+
+# Declaration of variables
+name = "Tom Collier"
+height = 7
+address = "33 Strathavon road"
+phoneNum = "021 123 123"
+allergy = False
+
+# Output to screen
+  
+    # Variable output value
+print(name, 'is:')
+print(height, "meters tall.")
+print('lives at',address)
+print('their phone number is', phoneNum)
+print('Has allergys:', allergy)
+
+print(name, ',', height, 'meters tall, lives at', address, ', phone number ', phoneNum, ', has allergys:', allergy)
+
+
+
+
+
