52a0ded3f126d0d64c11822aaa6e88faafddc633
TomCqr <103461338+TomCqr@users.noreply.github.com>
2022-08-18 12:18:54 +1200

    Add files via upload

---
+++ Lesson1/basic_var_and_output_shows_datattype_rewrite_numLegs.py
@@ -0,0 +1,27 @@
+# Tom Collier
+#
+# Declaration of variables
+
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True 
+
+# Output to screen
+    # Data type output
+print(type(name))
+print(type(numLegs))
+print(type(height))
+print(type(hasTail))
+
+    # Variable value output
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
+
+
+# Var changed
+numLegs = 3
+print(numLegs)
+
---
+++ Lesson1/basic_var_and_output_with_lables.py
@@ -0,0 +1,16 @@
+# Tom Collier
+#
+# Declaration of variables
+
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True 
+
+# Output to screen
+
+    # Variable value output
+print("The animal:", name)
+print("The number of legs:", numLegs)
+print("Height in meters:", height)
+print("It has a tail:", hasTail)
