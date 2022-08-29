4f70ad471ef0ef9f3a57c37e61078d124d398fab
TomCqr <103461338+TomCqr@users.noreply.github.com>
2022-08-30 09:06:02 +1200

    Add files via upload

--- Lesson1/python_practice_task_lesson1.py
+++ Lesson1/python_practice_task_lesson1.py
@@ -1,25 +1,75 @@
 # Tom Collier
-# Practice task
+# Practice task:
+# Variables, output, labels, data types, comments
 
-# Declaration of variables
+# Declaration of variables (draft)
 name = "Tom Collier"
-height = 7
+height = 1.8
+age = 14
+title = "Mr"
 address = "33 Strathavon road"
 phoneNum = "021 123 123"
 allergy = False
 
+
+
 # Output to screen
-  
-    # Variable output value
-print(name, 'is:')
-print(height, "meters tall.")
-print('lives at',address)
-print('their phone number is', phoneNum)
-print('Has allergys:', allergy)
 
-print(name, ',', height, 'meters tall, lives at', address, ', phone number ', phoneNum, ', has allergys:', allergy)
+    #summary printout
+print(name)
+print(height)
+print(age)
+print(address)
+print(phoneNum)
+print(allergy)
 
 
+# Clarafication of problem
+
+    # Decide on name variable
+fullName = "Tom Collier"
+firstName = "Tom"
+lastName = "Collier"
+title = "Mr"
+    
+# defime height measurment:
+# meters 
+height = 1.79
+
+# define adress 
+# seperate variables for:
+# street number
+# street name
+street = "strathavon road"
+streetNum = 33
+# Assume reader knows country, city, ect
+
+# phone landline or mobile
+phone = "021123123 "
+
+# Allergy - only define as having one but not the detail
+hasAllergy = False 
+# Comments help other programers understand what the code is doing 
+
+
+
+# Final variables
+firstName = "Tom"
+heightM = 1.8
+age = 14
+streetNum = 33
+streetName = "Strathavon Road"
+phoneNum = "021 123 123"
+allergy = False 
+
+# Output to screen 
+    # Display variables in sentence
+
+print("My name is", firstName, ", I am", age, "years old,", heightM, "meters tall. My adress is", streetNum, streetName, "my phone number is", phoneNum, "I have an allergy:", allergy)
+
+# Reflection:
+    # more specific variables 
+ 
 
 
 
