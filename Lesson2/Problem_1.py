0a5f752703f6fe6e0f2c9535453504d2906b2284
TomCqr <103461338+TomCqr@users.noreply.github.com>
2022-09-07 10:50:35 +1200

    Add files via upload

---
+++ Lesson2/Problem_1.py
@@ -0,0 +1,11 @@
+# Tom Collier
+# Cakculation of variables, framing of lables, data in context
+
+# Problam 01
+# This program is esigned to track and total goals scored by the Scots # XI for each game played
+
+match1_ST_Pats = 7
+match2_Well_Cole = 10
+match3_Well_High = 10
+Team_Total = match1_ST_Pats + match2_Well_Cole + match3_Well_High
+print("Total Goals Scored", Team_Total)
---
+++ Lesson2/user_input.py
@@ -0,0 +1,25 @@
+# Tom Collier
+# Input - various types by user 
+
+
+# input() function
+
+# Taking input from the user
+#string = input()
+# Output
+#pgint()
+
+#name = input("Enter your name: ")
+#print("Hello", name)
+
+
+# Taking input from the user as a float
+#num = float(input("Enter a number: "))
+#add = num + 1
+# Output
+#print(add)
+
+# Taking input fro a user as a list
+#li = list(input("Enter a number: "))
+# Output
+#print(li)
