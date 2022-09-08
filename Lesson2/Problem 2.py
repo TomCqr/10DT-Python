9e09ae349ea2468d87eb2d7f1b8717bbd311f86c
TomCqr <103461338+TomCqr@users.noreply.github.com>
2022-09-08 12:14:02 +1200

    Add files via upload

---
+++ Lesson2/Problem 2.py
@@ -0,0 +1,13 @@
+# Tom Collier
+# representing number data type as string 
+
+# Problem 2
+# This program is designed to track the value remaining on a gift card as it is used.
+
+Card_Balance = 50.00
+Purchase_01 = 35.00
+print("Your Current Balance is: $" + str (Card_Balance))
+
+Card_Balance = Card_Balance - Purchase_01
+print ("Purchase 01: $" + str (Purchase_01))
+print ("Your New Balance is: $" + str (Card_Balance))
---
+++ Lesson2/Problem 3.py
@@ -0,0 +1,21 @@
+# Tom Collier
+#
+
+# Problem 3 
+# cincatenation in output, '+' as a 
+
+dieselPrice = 1.47
+# Rember variable names cannot begin with a didgit 
+petrol95Price = 2.17
+petrol91Price = 2.06
+print("Initial price:")
+print("Diesel price $" + str (dieselPrice))
+print("95 price $" + str (petrol95Price))
+print("91 price $" + str (petrol91Price))
+dieselPrice = dieselPrice * 1.1
+petrol95Price = petrol95Price
+petrol91Price = petrol91Price
+print("Price increase of 10%:")
+print("Diesel price $" + str (dieselPrice))
+print("95 price $" + str (petrol95Price))
+print("91 price $" + str (petrol91Price))
